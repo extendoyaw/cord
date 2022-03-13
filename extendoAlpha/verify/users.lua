5734e17925c522b058c476adbea452b4f8d0f2f4
extendoyaw <101474880+extendoyaw@users.noreply.github.com>
2022-03-13 12:23:06 +0300

    Update users.lua

--- extendoAlpha/verify/users.lua
+++ extendoAlpha/verify/users.lua
@@ -1,1 +1,1 @@
-lolnet27 stoyack dreamer_ san43zz s3rkovv danyatopchik artem1223 descore ezbreez egorsash13 Max19872 max19872
+lolnet27 stoyack dreamer_ san43zz s3rkovv danyatopchik artem1223 descore ezbreez egorsash13 Max19872 max19872 maloneprod
