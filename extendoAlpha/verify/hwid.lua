7e47734bf9a962048bdd93725f742e2a43b605d6
extendoyaw <101474880+extendoyaw@users.noreply.github.com>
2022-03-21 19:42:01 +0300

    Update hwid.lua

--- extendoAlpha/verify/hwid.lua
+++ extendoAlpha/verify/hwid.lua
@@ -1,1 +1,1 @@
-kano8e0ax86 PAVELOL3f02x86 ok9e0bx86 ilabya502x86 dadiaa503x86 vortex9e0ax86 stwob9e09x86 gunde0802x86 ozpx0200x86 Gunge0802x86 artem9e09x86 wqeqweqweqwe
+kano8e0ax86 PAVELOL3f02x86 ok9e0bx86 ilabya502x86 dadiaa503x86 vortex9e0ax86 stwob9e09x86 gunde0802x86 ozpx0200x86 Gunge0802x86 artem9e09x86 dmitr5e03x86 wqeqweqweqwe
